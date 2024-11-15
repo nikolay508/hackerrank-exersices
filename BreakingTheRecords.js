34863c5c93944ea3642622eb9feadd9101a74b6c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-11-16 01:40:52 +0200

    Create BreakingTheRecords.js

---
+++ BreakingTheRecords.js
@@ -0,0 +1,21 @@
+function breakingRecords(scores) {
+    // Write your code here
+    let minScore = scores[0];
+    let maxScore = scores[0];
+    let minCount = 0;
+    let maxCount = 0;
+    
+    for(let each of scores){
+        if(each > maxScore){
+            maxScore = each;
+            maxCount++;
+        }else if(each < minScore){
+            minScore = each;
+            minCount++;
+        }
+    }
+    
+    let result = [maxCount, minCount]
+    return(result)
+    
+}

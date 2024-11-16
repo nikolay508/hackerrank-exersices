07892a489ee094aabf7b32c639a872f763770601
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-11-17 00:28:16 +0200

    Create CamelCase4.js

---
+++ CamelCase4.js
@@ -0,0 +1,36 @@
+function processData(input) {
+    //Enter your code here
+    let arrInput = input.split('\r\n');
+    let finalArr = [];
+    
+    for(let each of arrInput){
+        let [operation, symboll, word] = each.split(';');
+        
+        if(operation === 'S'){
+            if(symboll === 'M'){
+                word = word.slice(0 ,-2);
+            }
+            
+            word = word.split(/(?=[A-Z])/).map(w => w.toLowerCase()).join(' ');
+            finalArr.push(word);
+        }else if(operation === 'C'){
+            word = word.split(' ')
+            .map((word, index) => 
+            index === 0 
+            ? word.toLowerCase()
+            : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() ).join('');
+            
+            if(symboll === 'C'){
+                word = word.charAt(0).toUpperCase() + word.slice(1);
+            }else if(symboll === 'M'){
+                word += '()';
+            }
+            
+            finalArr.push(word);
+        }
+    }
+    
+    for(let each of finalArr){
+        console.log(each);
+    }
+} 

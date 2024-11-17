5b5fd793e93defd4f6aebf7812200862eed6e481
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-11-18 00:08:57 +0200

    Create SparseArrays.js

---
+++ Problem Solving/SparseArrays.js
@@ -0,0 +1,11 @@
+function matchingStrings(strings, queries) {
+    // Write your code here
+    let finalArr = [];
+    
+    for(let query of queries){
+        const filtered = strings.filter(str => query === str);
+        finalArr.push(filtered.length)
+    }
+    
+    return finalArr
+}

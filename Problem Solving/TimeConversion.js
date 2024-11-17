96b178ba654567dfd2cc7d7219977d4cd10594cf
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-11-18 00:13:54 +0200

    Update TimeConversion.js

--- Problem Solving/TimeConversion.js
+++ Problem Solving/TimeConversion.js
@@ -1,17 +1,19 @@
 function timeConversion(s) {
     // Write your code here
-    let [hour, min, sec] = s.split(':');
-    hour = Number(hour);
+    let [hour, min, sec] = s.split(':')
+   
     
     let timeFrame = sec.slice(-2);
     sec = sec.slice(0, -2);
     
     if(timeFrame === 'AM' && hour >= 12){
+        hour = Number(hour);
         hour -= 12;
         hour = `0${hour}`;
     }else if(timeFrame === 'PM' && hour < 12){
+        hour = Number(hour);
         hour += 12;
     }
     
-    return `${hour}:${min}:${sec}`;
+    return `${hour}:${min}:${sec}`
 }

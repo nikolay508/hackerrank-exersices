21984bd0158cea327ddf95be2247501fbb4d336c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-11-18 18:53:20 +0200

    Update MockTest - 01.js

--- Problem Solving/MockTest - 01.js
+++ Problem Solving/MockTest - 01.js
@@ -1,5 +1,5 @@
 function findMedian(arr) {
 // Write your code here
-arr = arr.sort((a, b) => a - b);
-return(arr[(arr.length - 1)/2]);
+  arr = arr.sort((a, b) => a - b);
+  return(arr[(arr.length - 1)/2]);
 }

0bc9b044017a9a5142c226483704739545c4190c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-11-19 22:02:44 +0200

    Create GradingStudents.js

---
+++ Problem Solving/GradingStudents.js
@@ -0,0 +1,16 @@
+function gradingStudents(grades) {
+    // Write your code here
+    let finalGrade = [];
+    
+    for(let each of grades){     
+        let closestBy5 = Math.ceil(each / 5) * 5;
+         
+        if((closestBy5 - each) < 3 && each >= 38){
+            finalGrade.push(closestBy5);
+        }else{
+            finalGrade.push(each);
+        }
+    }
+    
+    return finalGrade;
+}

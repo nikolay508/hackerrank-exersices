7b1b5c446a2aca5279243075ea6c1313686e8b9e
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-11-19 22:01:49 +0200

    Create LonelyInteger.js

---
+++ Problem Solving/LonelyInteger.js
@@ -0,0 +1,8 @@
+function lonelyinteger(a) {
+    // Write your code here
+    let result = 0;
+    for (let num of a) {
+        result ^= num; // XOR each number with the result
+    }
+    return result;
+}
